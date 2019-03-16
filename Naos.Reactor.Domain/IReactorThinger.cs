4dd341bf96db8aa24cb88d8d0b4c10a85eaac0d8
Lawson Caudill <lawson@caudillclan.us>
2019-03-16 18:19:58 -0300

    Added solution with starter class to collect ideas.

---
+++ Naos.Reactor.Domain/IReactorThinger.cs
@@ -0,0 +1,12 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IReactorThinger.cs" company="Naos Project">
+//    Copyright (c) Naos Project 2019. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Naos.Reactor.Domain
+{
+    public interface IReactorThinger
+    {
+    }
+}
