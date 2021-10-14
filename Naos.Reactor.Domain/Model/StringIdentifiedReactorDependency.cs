e935e00a0ac3ca1cc06cec5607dd46ea33a21885
Lawson Caudill <lawson@getthinktank.com>
2021-10-13 21:43:48 -0400

    Added a generic string id'ed IReactorDependency.

---
+++ Naos.Reactor.Domain/Model/StringIdentifiedReactorDependency.cs
@@ -0,0 +1,47 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="StringIdentifiedReactorDependency.cs" company="Naos Project">
+//    Copyright (c) Naos Project 2019. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Naos.Reactor.Domain
+{
+    using Naos.Database.Domain;
+    using OBeautifulCode.Assertion.Recipes;
+    using OBeautifulCode.Type;
+
+    /// <summary>
+    /// Job dependency interface.
+    /// </summary>
+    public class StringIdentifiedReactorDependency : IReactorDependency, IHaveStringId, IModelViaCodeGen
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StringIdentifiedReactorDependency"/> class.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <param name="streamRepresentation">The <see cref="IStreamRepresentation"/> that will resolve to a <see cref="IStream"/> that should examined for events to produce a <see cref="ReactionEvent"/>.</param>
+        /// <param name="tryHandleRecordOp">The <see cref="TryHandleRecordOp"/> to execute against the <see cref="IStream"/> which, if any events are handled, will produce a <see cref="ReactionEvent"/>.</param>
+        public StringIdentifiedReactorDependency(
+            string id,
+            IStreamRepresentation streamRepresentation,
+            TryHandleRecordOp tryHandleRecordOp)
+        {
+            id.MustForArg(nameof(id)).NotBeNullNorWhiteSpace();
+            streamRepresentation.MustForArg(nameof(streamRepresentation)).NotBeNull();
+            tryHandleRecordOp.MustForArg(nameof(tryHandleRecordOp)).NotBeNull();
+
+            this.Id = id;
+            this.StreamRepresentation = streamRepresentation;
+            this.TryHandleRecordOp = tryHandleRecordOp;
+        }
+
+        /// <inheritdoc />
+        public string Id { get; private set; }
+
+        /// <inheritdoc />
+        public IStreamRepresentation StreamRepresentation { get; private set; }
+
+        /// <inheritdoc />
+        public TryHandleRecordOp TryHandleRecordOp { get; private set; }
+    }
+}
