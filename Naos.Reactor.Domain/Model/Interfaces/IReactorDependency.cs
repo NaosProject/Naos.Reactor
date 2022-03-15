40f84ffa1786fa6f5da655aa29d10e95db92beb4
Lawson Caudill <lawson@getthinktank.com>
2022-03-15 15:08:14 -0400

    CA fix.

--- Naos.Reactor.Domain/Model/Interfaces/IReactorDependency.cs
+++ Naos.Reactor.Domain/Model/Interfaces/IReactorDependency.cs
@@ -6,9 +6,12 @@
 
 namespace Naos.Reactor.Domain
 {
+    using Naos.CodeAnalysis.Recipes;
+
     /// <summary>
     /// Interface for dependencies of a <see cref="ReactionRegistration"/>.
     /// </summary>
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1040:AvoidEmptyInterfaces", Justification = NaosSuppressBecause.CA1040_AvoidEmptyInterfaces_NeedToIdentifyGroupOfTypesAndPreferInterfaceOverAttribute)]
     public interface IReactorDependency
     {
     }
