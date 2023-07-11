0f390a881a0b5270ff74379c194202fc0418b691
Lawson Caudill <1660562+wlscaudill@users.noreply.github.com>
2023-07-10 23:41:13 -0400

    Added some parallel hardening to ensure we do not lose processing without knowing it.

--- Naos.Reactor.Domain/Protocols/RunReactorProtocol.cs
+++ Naos.Reactor.Domain/Protocols/RunReactorProtocol.cs
@@ -69,67 +69,115 @@ namespace Naos.Reactor.Domain
                                        {
                                            operation.DeprecatedIdentifierType
                                        }));
-            var distinctIds = this.reactionRegistrationStream.Execute(getDistinctStringSerializedIdsOp);
-            Parallel.ForEach(distinctIds,
-                new ParallelOptions { MaxDegreeOfParallelism = operation.DegreesOfParallelismForDependencyChecks },
-                distinctId =>
+
+            var processedIds = new ConcurrentDictionary<StringSerializedIdentifier, Exception>();
+            void ProcessReactionRegistration(
+                StringSerializedIdentifier reactionRegistrationId)
+            {
+                try
                 {
-                    var found = ReactionRegistrationIdToNextEvaluationCutoffMap.TryGetValue(distinctId, out var nextEvaluationCutoff);
+                    var getLatestRecordOp = new StandardGetLatestRecordOp(
+                        new RecordFilter(
+                            ids: new[]
+                                 {
+                                     reactionRegistrationId,
+                                 }));
 
-                    if (!found || DateTime.UtcNow > nextEvaluationCutoff)
+                    var reactionRegistrationRecord = this.reactionRegistrationStream.Execute(getLatestRecordOp);
+                    reactionRegistrationRecord
+                       .Payload
+                       .PayloadTypeRepresentation
+                       .RemoveAssemblyVersions()
+                       .MustForOp("recordFromReactionRegistrationStreamExpectedToBeRegisteredReaction")
+                       .BeEqualTo(ReactionRegistrationTypeRepWithoutVersion);
+
+                    var reactionRegistration =
+                        reactionRegistrationRecord.Payload.DeserializePayloadUsingSpecificFactory<ReactionRegistration>(
+                            this.reactionRegistrationStream.SerializerFactory);
+
+                    var evaluateReactionRegistrationOp = new EvaluateReactionRegistrationOp(reactionRegistration);
+                    var evaluateReactionRegistrationResult =
+                        this.evaluateReactionRegistrationProtocol.Execute(evaluateReactionRegistrationOp);
+                    if (evaluateReactionRegistrationResult.ReactionEvent != null)
                     {
-                        try
+                        var reaction = evaluateReactionRegistrationResult.ReactionEvent;
+
+                        this.reactionStream.PutWithId(reaction.Id, reaction, reaction.Tags);
+
+                        // once we have recorded the reaction then we can finalize the handling cycle.
+                        foreach (var recordSetHandlingMemento in evaluateReactionRegistrationResult.RecordSetHandlingMementos)
                         {
-                            var getLatestRecordOp = new StandardGetLatestRecordOp(
-                                new RecordFilter(
-                                    ids: new[]
-                                         {
-                                             distinctId,
-                                         }));
-
-                            var reactionRegistrationRecord = this.reactionRegistrationStream.Execute(getLatestRecordOp);
-                            reactionRegistrationRecord
-                               .Payload
-                               .PayloadTypeRepresentation
-                               .RemoveAssemblyVersions()
-                               .MustForOp("recordFromReactionRegistrationStreamExpectedToBeRegisteredReaction")
-                               .BeEqualTo(ReactionRegistrationTypeRepWithoutVersion);
-
-                            var reactionRegistration =
-                                reactionRegistrationRecord.Payload.DeserializePayloadUsingSpecificFactory<ReactionRegistration>(
-                                    this.reactionRegistrationStream.SerializerFactory);
-
-                            var evaluateReactionRegistrationOp = new EvaluateReactionRegistrationOp(reactionRegistration);
-                            var evaluateReactionRegistrationResult =
-                                this.evaluateReactionRegistrationProtocol.Execute(evaluateReactionRegistrationOp);
-                            if (evaluateReactionRegistrationResult.ReactionEvent != null)
-                            {
-                                var reaction = evaluateReactionRegistrationResult.ReactionEvent;
-
-                                this.reactionStream.PutWithId(reaction.Id, reaction, reaction.Tags);
-
-                                // once we have recorded the reaction then we can finalize the handling cycle.
-                                foreach (var recordSetHandlingMemento in evaluateReactionRegistrationResult.RecordSetHandlingMementos)
-                                {
-                                    recordSetHandlingMemento.CompleteSet();
-                                }
-                            }
-
-                            var newNextEvaluationCutoff = DateTime.UtcNow.Add(reactionRegistration.IdealWaitTimeBetweenEvaluations);
-                            ReactionRegistrationIdToNextEvaluationCutoffMap.AddOrUpdate(
-                                distinctId,
-                                newNextEvaluationCutoff,
-                                (_, __) => newNextEvaluationCutoff);
-                        }
-                        catch (Exception ex)
-                        {
-                            throw new ReactorException(
-                                Invariant($"Failed to process {nameof(ReactionRegistration)} Id: {distinctId}."),
-                                ex,
-                                operation);
+                            recordSetHandlingMemento.CompleteSet();
                         }
                     }
-                });
+
+                    var newNextEvaluationCutoff = DateTime.UtcNow.Add(reactionRegistration.IdealWaitTimeBetweenEvaluations);
+                    ReactionRegistrationIdToNextEvaluationCutoffMap.AddOrUpdate(
+                        reactionRegistrationId,
+                        newNextEvaluationCutoff,
+                        (
+                            _,
+                            __) => newNextEvaluationCutoff);
+                    processedIds.TryAdd(reactionRegistrationId, null);
+                }
+                catch (Exception ex)
+                {
+                    processedIds.TryAdd(reactionRegistrationId, ex);
+                    throw new ReactorException(
+                        Invariant($"Failed to process {nameof(ReactionRegistration)} Id: {reactionRegistrationId}."),
+                        ex,
+                        operation);
+                }
+            }
+
+            var distinctIds = this.reactionRegistrationStream.Execute(getDistinctStringSerializedIdsOp);
+            var idsToProcess = new List<StringSerializedIdentifier>();
+            foreach (var idToCheck in distinctIds)
+            {
+                var found = ReactionRegistrationIdToNextEvaluationCutoffMap.TryGetValue(idToCheck, out var nextEvaluationCutoff);
+
+                if (!found || DateTime.UtcNow > nextEvaluationCutoff)
+                {
+                    idsToProcess.Add(idToCheck);
+                }
+            }
+
+            var parallelLoopResult = Parallel.ForEach(idsToProcess,
+                new ParallelOptions { MaxDegreeOfParallelism = operation.DegreesOfParallelismForDependencyChecks },
+                ProcessReactionRegistration);
+
+            // this should only be necessary if stop or break is called which is not happening but is here from a paranoid perspective...
+            parallelLoopResult.IsCompleted.MustForOp(nameof(parallelLoopResult)).BeTrue();
+
+            if (processedIds.Count != idsToProcess.Count)
+            {
+                var missedIds = idsToProcess.Where(_ => !processedIds.ContainsKey(_)).ToList();
+                var missedIdsMixIn = string.Join(",", missedIds);
+                throw new ReactorException(
+                    Invariant($"Failed to process {nameof(ReactionRegistration)} Missed Ids: {missedIdsMixIn}."),
+                    operation);
+            }
+
+            if (processedIds.Any(_ => _.Value != null))
+            {
+                var exceptions = processedIds
+                                .Where(_ => _.Value != null)
+                                .Select(
+                                     _ =>
+                                         new ReactorException(
+                                             Invariant(
+                                                 $"Failed to process {nameof(ReactionRegistration)} Id: {_.Key} had unreported exception: {_.Value}."),
+                                             operation))
+                                .ToList();
+                if (exceptions.Count == 1)
+                {
+                    throw exceptions.Single();
+                }
+                else
+                {
+                    throw new AggregateException(exceptions);
+                }
+            }
         }
     }
 }
